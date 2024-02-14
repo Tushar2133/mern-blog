f6d9c5a56d6c62a376fc01a73fd663c297361d85
Tushar <tusharsanadhya2133@gmail.com>
2024-02-14 14:16:22 +0530

    create and add footer component

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,23 +1,25 @@
-import { BrowserRouter,Route,Routes } from "react-router-dom";
-import Home from './pages/Home'
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Home from "./pages/Home";
 import About from "./pages/About";
-import Signin from './pages/Signin'
+import Signin from "./pages/Signin";
 import Signup from "./pages/Signup";
 import Dashboard from "./pages/Dashboard";
 import Projects from "./pages/Projects";
-import Header from './components/Header';
+import Header from "./components/Header";
+import Footer from "./components/footer";
 export default function App() {
   return (
-   <BrowserRouter>
-    <Header/>
-    <Routes>
-      <Route path="/" element={<Home/>}/>
-      <Route path="/about" element={<About/>}/>
-      <Route path="/signin" element={<Signin/>}/>
-      <Route path="/signup" element={<Signup/>}/>
-      <Route path="/dashboard" element={<Dashboard/>}/>
-      <Route path="/projects" element={<Projects/>}/>
-    </Routes>
-   </BrowserRouter>
+    <BrowserRouter>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/signin" element={<Signin />} />
+        <Route path="/signup" element={<Signup />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/projects" element={<Projects />} />
+      </Routes>
+      <Footer />
+    </BrowserRouter>
   );
 }
